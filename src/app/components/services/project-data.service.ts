94a99eb01139be7df65f6d6bb5930f0ef8ba23b8
griseldaalegre <alegregriselda5@gmail.com>
2024-07-22 12:42:18 -0300

    Modificaciones en project-data

--- src/app/components/services/project-data.service.ts
+++ src/app/components/services/project-data.service.ts
@@ -10,19 +10,19 @@ export class ProjectDataService {
 
   projects: Project[] = [
     {
-      name: "Project 1",
-      description: "Description of Project 1",
-      linkRepository: "https://github.com/user/project1",
+      name: "Proyecto seminario",
+      description: "Web de adopción de mascotas. Los usuarios pueden explorar una lista de mascotas disponibles para adopción, ver detalles específicos de cada mascota y agregarla a una lista de favoritos.",
+      linkRepository: "https://github.com/griseldaalegre/proyecto-seminario-angular",
       img: "path/to/image1.jpg"
     },
     {
-      name: "Project 2",
-      description: "Description of Project 2",
+      name: "Biblioteca",
+      description: " Biblioteca donde se almacenan libros y se registran usuarios para el préstamo de los mismos. En el proyecto se implementaron tres tipos de usuarios, usuario administrador, usuario normal, usuario publico.",
       linkRepository: "https://github.com/user/project2",
       img: "path/to/image2.jpg"
     },
     {
-      name: "Project 1",
+      name: "Portafolio",
       description: "Description of Project 1",
       linkRepository: "https://github.com/user/project1",
       img: "path/to/image1.jpg"
